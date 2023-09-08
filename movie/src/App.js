45ee6b8ea06d7ea9ec02384d924903ee139a01c4
iramkhan123 <iramxyzkhan@gmail.com>
2023-09-09 00:57:05 +0530

    react-router-dom

--- movie/src/App.js
+++ movie/src/App.js
@@ -1,18 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
 import Navbar from './Components/Navbar';
-//import Banner from './Components/Banner';
-//import List from './Components/List';
+import Banner from './Components/Banner';
+import List from './Components/List';
 import Favourites from './Components/Favourites';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <>
-    <Navbar/>
-    {/*<Banner/>
-    <List/>*/}
-    <Favourites/>
-    </>
+    <BrowserRouter>
+      <Navbar/>
+        <Routes>
+          <Route path="/" element={<List/>} />
+          <Route path="/fav" element={<Favourites/>} />
+        </Routes>
+  
+    </BrowserRouter>
   );
 }
 
--- movie/src/Components/Navbar.js
+++ movie/src/Components/Navbar.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-
+import { Link } from "react-router-dom";
 export default class 
  extends Component {
   render() {
@@ -11,10 +11,13 @@ export default class
           padding: "1rem",
    
         }}
-      >
+      > 
+      <Link to="/" style={{textDecoration:"none"}}>
         <h3 className="decor">Movie App</h3>
+      </Link>
+      <Link to="/fav" style={{textDecoration:"none"}}>
         <h3 style={{ marginLeft: "2rem" }} className="decor">Favourites</h3>
-
+        </Link>
       </div>
     )
   }
